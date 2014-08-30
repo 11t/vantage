a1c54a066b0f0ac40d00be65faff7dd3993fdf4c
11t <eleventea11@gmail.com>
2014-08-29 18:06:19 -0700

    Added osu default easing curves

--- Vantage/Animation3D/Animation/EasingCurves/BasicEasingCurve.cs
+++ Vantage/Animation3D/Animation/EasingCurves/BasicEasingCurve.cs
@@ -4,5 +4,22 @@ namespace Vantage.Animation3D.Animation.EasingCurves
     {
         public static readonly StepEasingCurve Step = new StepEasingCurve();
         public static readonly LinearEasingCurve Linear = new LinearEasingCurve();
+        public static readonly OsuEaseInEasingCurve OsuEaseIn = new OsuEaseInEasingCurve();
+        public static readonly OsuEaseOutEasingCurve OsuEaseOut = new OsuEaseOutEasingCurve();
+
+        public static IEasingCurve FromEasingParameter(int easing)
+        {
+            switch (easing)
+            {
+                case 0:
+                    return Linear;
+                case 1:
+                    return OsuEaseOut;
+                case 2:
+                    return OsuEaseIn;
+                default:
+                    return Step;
+            }
+        }
     }
 }
